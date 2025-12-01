c351a65f6530f83ab4ae08cf9729e53267b0bdd7
WizardofOzze <ozzyv@nycstudents.net>
2025-12-01 14:44:13 -0500

    Update bm0_madlibs.py

--- bm0_madlibs.py
+++ bm0_madlibs.py
@@ -1,5 +1,14 @@
-print("Hello World")
+import random 
 
+
+name = input("Whats your name?")
+sport = input("Whats your favorite sport?")
+team = input("Whats your favorite team?")
+
+if random.randint(2) == 2:	
+	print(f"My name is {name}, my favorite sport is {sport}, and my favorite team is {team}")
+else:
+	print(f"I dont like {name}, because they like to play {sport}, and support a team I hate, being {team}.")
 
 
 
